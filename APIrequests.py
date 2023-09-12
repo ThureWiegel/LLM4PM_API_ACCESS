e2eaa1b39d90dafcdba000bf3607b3d13f9c6502
ThureWiegel <thurewiegel@gmail.com>
2023-09-13 00:59:52 +0200

    Added Classifier and extractor API functions

--- APIrequests.py
+++ APIrequests.py
@@ -1,22 +1,116 @@
 import openai
+import json
 
 API_KEY = open("API KEY", "r").read()
 openai.api_key = API_KEY
 
 
-def gpt3_chat(message):
+def gpt_classifier(message):
+    function = [
+        {
+            "name": "classify_Email",
+            "description": "gets the datetime, subject company and subject object from the email",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "datetime": {
+                        "type": "string",
+                        "description": "the datetime of the email."
+                                       "format like this: 2021-01-01 00:00:00."
+                                       "If no datetime is found, return 'none'"
+                    },
+                    "company": {
+                        "type": "string",
+                        "description": "If no company is found, return 'none'"
+                                       "the company mentioned in the subject line, usually formatted like: XX for company YY."
+                                       ""
+                                       "always return company"
+                    },
+                    "object": {
+                        "type": "string",
+                        "description": "If no object is found, return 'none'."
+                                       "the object mentioned in the subject line, usually formatted like: object XX for company YY."
+                                       ""
+                                       "always return object"
+                    }
+                }
+            }
+        }
+    ]
 
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
             {
-                "role": "user",
-                "content": message
+                "role": "system",
+                "content": "You are a email analyser. You are given an email and reliably filter out information within it."
             },
-        ]
+            {
+                "role": "user",
+                "content": f"Here is an email: {message}, please return the datetime, subject company and subject object from the email."
+            }
+        ],
+        functions=function,
     )
 
-    result = response['choices'][0]['message']['content'].strip()
+    result = response['choices'][0]['message']['function_call']['arguments']
+    json_result = json.loads(result)
+
     tokens = response['usage']['total_tokens']
 
-    return result, tokens
+    # print(json_result)
+
+    return json_result["datetime"], json_result["company"], json_result["object"], tokens
+
+
+def gpt_extractor(message):
+    function = [
+        {
+            "name": "email_summarizer",
+            "description": "give a summary of each relevant point talked about in the email",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "summarization": {
+                        "type": "string",
+                        "description": "summarization"
+                                       "formatted like this: "
+                                       "1. talking point 1"
+                                       "2. talking point 2"
+                                       "3. talking point 3"
+                                       "..."
+                                       "If no talking points are found, return 'none'"
+                                       "disregard talking points that is not relevant to the subject company and subject object"
+                                       "disregard talking points that directly regard the email itself"
+                    }
+                }
+            }
+        }
+    ]
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a email analyser. You are given an email and reliably filter out information within it."
+            },
+            {
+                "role": "user",
+                "content": f"Here is an email: {message}. Please summarize each talking point in the email separately"
+            }
+        ],
+        functions=function,
+    )
+
+    result = response['choices'][0]['message']['function_call']['arguments']
+    # print(response)
+    json_result = json.loads(result, strict=False)
+
+    # print(json_result)
+
+    tokens = response['usage']['total_tokens']
+
+    # print(json_result)
+
+    return json_result["summarization"], tokens
--- main.py
+++ main.py
@@ -1,26 +1,23 @@
-from APIrequests import gpt3_chat
+import APIrequests
 import os
 
 processedDir = 'D:/Coding Projects/Python/LLM4PM/processed'
 tokensUsed = 0
 
-prompt = "give me all people, companies and products discussed in the email, format them like this: {property}:{name}. " \
-         "Properties include: sender, receiver, product, company, other" \
-         "Each entry as individual lines, no empty lines."
-
 for count, file in enumerate(os.listdir(processedDir)):
+    # calls the classifier function from APIrequests.py with the email as the argument
+    result = (APIrequests.gpt_classifier(str(open(processedDir + '/' + file, 'r', encoding='utf-8').read())))
 
-    # for each file , call the gpt3_chat function and give a string as input
-    # the string should be "give me the line number at which each individual email begins" + the contents of the file
-    # the output should be a string that is the line number at which each individual email begins
-    # print the output
-    result = (gpt3_chat(prompt + "\n\n" + str(open(processedDir + '/' + file, 'r',encoding='utf-8').read())))
+    # tokensUsed += result[3]
 
-    tokensUsed += result[1]
+    print(result[0])
+    print(result[1])
+    print(result[2])
+    print()
 
-    print(str(count+1) + "/" + str(len(os.listdir(processedDir))) + " - " + str(file) + " - " + str(result[1]) + "/" + str(tokensUsed))
-    print(result[0] + "\n")
 
-print("Total tokens used: " + str(tokensUsed))
-print("price: " + str(tokensUsed/1000 * 0.0015) + " USD")
-print("average tokens: " + str(tokensUsed/len(os.listdir(processedDir))))
+
+
+print("\nTotal tokens used: " + str(tokensUsed))
+print("price: " + str(tokensUsed / 1000 * 0.0015) + " USD")
+print("average tokens: " + str(tokensUsed / len(os.listdir(processedDir))))
